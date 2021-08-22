f05f34ccb8ab9c62c4e6857501a178c8974cd744
Nathan Conroy <nathan.conroy@protonmail.com>
2021-08-22 14:22:23 -0500

    Create Config struct and parse args into it.

---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+
+
+pub struct Config {
+    pub file_nm: String    
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use std::env;
 
 fn main() {
-    println!("{}", parse_args());
+    let config = parse_args();
+    println!("{}", config.file_nm);
 }
 
 
-fn parse_args() -> String {
+fn parse_args() -> textual::Config {
     // Returns the path to the file to edit.
     let args: Vec<String> = env::args().collect();
-    return String::from(&args[1]);
+    textual::Config {
+        file_nm: String::from(&args[1]),
+    }
 }
