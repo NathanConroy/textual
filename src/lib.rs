b74177f3b509260aa6f1cb28ee8c24be1ee92e07
Nathan Conroy <nathan.conroy@protonmail.com>
2021-08-22 14:36:24 -0500

    Create Config::new function. Test.

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,23 @@
 pub struct Config {
     pub file_nm: String    
 }
+
+impl Config {
+    pub fn new(args: Vec<String>) -> Self {
+        Self {
+            file_nm: String::from(&args[1]),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn config_args() {
+        let pnm = String::from("textual");
+        let fnm = String::from("file_nm");
+        let config = Config::new(vec![pnm, fnm]);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,7 @@
 use std::env;
 
 fn main() {
-    let config = parse_args();
-    println!("{}", config.file_nm);
-}
-
-
-fn parse_args() -> textual::Config {
-    // Returns the path to the file to edit.
     let args: Vec<String> = env::args().collect();
-    textual::Config {
-        file_nm: String::from(&args[1]),
-    }
+    let config = textual::Config::new(args);
+    println!("{}", config.file_nm);
 }
