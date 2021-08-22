aa97f6a0f4107d42dfbe68b7f4c6596609d4e454
Nathan Conroy <nathan.conroy@protonmail.com>
2021-08-22 14:18:14 -0500

    Get file name from prog args.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use std::env;
 
 fn main() {
+    println!("{}", parse_args());
+}
+
+
+fn parse_args() -> String {
+    // Returns the path to the file to edit.
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    return String::from(&args[1]);
 }
