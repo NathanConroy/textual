5ebdbec55565ee854f5882767eef1895e5ab0ba0
Nathan Conroy <nathan.conroy@protonmail.com>
2021-08-22 15:00:06 -0500

    Add error-handling for having too few args in Config::new

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,16 @@
 
-
 pub struct Config {
     pub file_nm: String    
 }
 
 impl Config {
-    pub fn new(args: Vec<String>) -> Self {
-        Self {
-            file_nm: String::from(&args[1]),
+    pub fn new(args: Vec<String>) -> Result<Self, String> {
+        if args.len() < 2 {
+            return Err(String::from("Expected more arguments."));
         }
+        Ok(Self {
+            file_nm: String::from(&args[1]),
+        })
     }
 }
 
@@ -17,9 +19,17 @@ mod tests {
     use super::*;
 
     #[test]
-    fn config_args() {
+    fn can_init_config() {
         let pnm = String::from("textual");
         let fnm = String::from("file_nm");
-        let config = Config::new(vec![pnm, fnm]);
+        let result = Config::new(vec![pnm, fnm]);
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn too_few_args() {
+        let pnm = String::from("textual");
+        let result = Config::new(vec![pnm]);
+        assert!(result.is_err());
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use std::env;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = textual::Config::new(args);
+    let config = textual::Config::new(args).unwrap_or_else(|err_msg| {
+        println!("{}", err_msg);
+        process::exit(1);
+    });
     println!("{}", config.file_nm);
 }
