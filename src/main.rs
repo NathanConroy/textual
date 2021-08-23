a7aee7e1f082371b6a5b58e4918a1c4db9c412a0
Nathan Conroy <nathan.conroy@protonmail.com>
2021-08-22 22:23:36 -0500

    Move config code to config.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::env;
 use std::process;
 
+mod config;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = textual::Config::new(args).unwrap_or_else(|err_msg| {
+    let config = config::Config::new(args).unwrap_or_else(|err_msg| {
         println!("{}", err_msg);
         process::exit(1);
     });
