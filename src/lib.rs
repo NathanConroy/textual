3c9ddd3b3f9e7bf62410006d4b452fee911299cc
Nathan Conroy <nathan.conroy@protonmail.com>
2021-08-23 21:56:14 -0500

    Add ncurses package to project. Display file contents with ncurses.

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,35 @@
+extern crate ncurses;
+
 use std::fs;
 
 
 pub fn run(file_nm: String) {
     let contents = read_file(file_nm);
+    ncurses::initscr();
     loop {
         draw(contents);
+        let ch = get_user_input();
+        println!("ch {}", ch);
         break;
     }
+    ncurses::endwin();
 }
 
+
 fn read_file(file_nm: String) -> String {
-    println!("Opening {}.\n", file_nm);
     fs::read_to_string(file_nm)
         .expect("There was a problem reading this file.")
 }
 
+
 fn draw(contents: String) {
-    println!("Contents:\n\n{}", contents);
+    ncurses::addstr(
+        format!("Contents:\n\n{}", contents).as_str()
+    );
+}
+
+
+fn get_user_input() -> i32 {
+    ncurses::getch()
 }
 
